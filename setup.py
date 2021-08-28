f6641701f2b2c92170e726954da236f0476d729a
Chris <44866753+kuelumbus@users.noreply.github.com>
2021-08-28 11:23:03 -0400

    Update

--- setup.py
+++ setup.py
@@ -164,7 +164,7 @@ class BuildRDKit(build_ext_orig):
 
         # Invoke cmake and compile RDKit
         options = [
-                    f"-DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake" if sys.platform == 'win32' else "",
+                    f"-DCMAKE_TOOLCHAIN_FILE=C:\\vcpkg\\scripts\\buildsystems\\vcpkg.cmake" if sys.platform == 'win32' else "",
 
                     f'-DPYTHON_EXECUTABLE={sys.executable}',
                     f'-DPYTHON_INCLUDE_DIR={get_paths()["include"]}',
@@ -175,10 +175,13 @@ class BuildRDKit(build_ext_orig):
                     f"-DRDK_BUILD_PYTHON_WRAPPERS=ON",
                     f"-DRDK_INSTALL_INTREE=OFF",
                                    
-                    f"-DBOOST_ROOT={boost_install_path}" if sys.platform != 'win32' else "",
+                    f"-DBOOST_ROOT={boost_install_path}",
+                    f"-DBOOST_LIBRARYDIR={boost_install_path}\lib" if sys.platform == 'win32' else "",
+                    f"-DBOOST_INCLUDEDIR={boost_install_path}\include" if sys.platform == 'win32' else "",
+                    
 
-                    f"-DBoost_INCLUDE_DIRS={boost_install_path / 'include'}" if sys.platform == 'win32' else "",
-                    f"-DBoost_LIBRARY_DIRS={boost_install_path / 'lib'}" if sys.platform == 'win32' else "",
+                    # f"-DBoost_INCLUDE_DIRS={boost_install_path / 'include'}" if sys.platform == 'win32' else "",
+                    # f"-DBoost_LIBRARY_DIRS={boost_install_path / 'lib'}" if sys.platform == 'win32' else "",
                     
                     f"-DBoost_NO_SYSTEM_PATHS=ON",
             
