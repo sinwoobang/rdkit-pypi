00aa3805885eb9b01d386ca1fc52338c03cb42e8
Chris <44866753+kuelumbus@users.noreply.github.com>
2021-08-31 10:50:17 -0400

    aaaaaa

--- setup.py
+++ setup.py
@@ -167,7 +167,7 @@ class BuildRDKit(build_ext_orig):
             
             cmds = [                
                 f'./b2 address-model=64 architecture=x86 link=static link=shared threading=single threading=multi ' \
-                f'variant=release --build-type=complete' \
+                f'variant=release --build-type=complete ' \
                 # f'--with-python --with-serialization --with-iostreams --with-system --with-regex --with-program_options ' \
                 f'--prefix={boost_install_path} -j 20 install',
             ]
