1e6006610d2dbfb998f5ddad24e221f1dc920a5b
Chris <44866753+kuelumbus@users.noreply.github.com>
2021-09-04 16:32:35 -0400

    fix

--- setup.py
+++ setup.py
@@ -279,8 +279,8 @@ class BuildRDKit(build_ext_orig):
             "-Ax64" if sys.platform == 'win32' else "",
 
             # Mac needs these to compile 
-            f"-DCMAKE_C_FLAGS=-Wno-implicit-function-declaration" if sys.platform != 'win32' else "", # "-DCMAKE_C_FLAGS=/d2FH4-",
-            f"-DCMAKE_CXX_FLAGS=-Wno-implicit-function-declaration" if sys.platform != 'win32' else "", #"-DCMAKE_CXX_FLAGS=/d2FH4-",
+            f"-DCMAKE_C_FLAGS=-Wno-implicit-function-declaration" if sys.platform == 'darwin' else "", 
+            f"-DCMAKE_CXX_FLAGS=-Wno-implicit-function-declaration" if sys.platform == 'darwin' else "", 
 
             f"-DCMAKE_INSTALL_PREFIX={rdkit_install_path}",
             f"-DCMAKE_BUILD_TYPE=Release",
@@ -288,7 +288,7 @@ class BuildRDKit(build_ext_orig):
         
         cmds = [
             f"cmake -S . -B build {' '.join(options)} ",
-            f"cmake --build build --verbose -j 20 --config Release",
+            f"cmake --build build -j 10 --config Release",
             f"cmake --install build"
         ]    
         [check_call(c.split()) for c in cmds]
