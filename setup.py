93d7fef7c5d5dea4865c0774bac9e89c485859d4
Chris <christopher.kuenneth@gmail.com>
2021-12-14 13:04:17 -0500

    ooo

--- setup.py
+++ setup.py
@@ -305,10 +305,11 @@ class CMakeBuild(build_ext_orig):
         
         # install correct boost-python version
         cmd = ["sed", "-i.bak"]
-        cmd += [f'/file(GLOB python3_include_dir/c\file(GLOB python3_include_dir "{python_include_dir}")']
+        cmd += [f'/file(GLOB python3_include_dir/c\    file(GLOB python3_include_dir "{python_include_dir}")']
         cmd += [b2_options]
 
         check_call(cmd)
+        check_call(f"cat {b2_options}".split())
         check_call(f"{os.environ['VCPKG_ROOT']}/vcpkg install".split())
         check_call(f"mv {b2_options}.bak {b2_options}".split())
         
